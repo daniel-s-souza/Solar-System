708ed243166c55e32b567adbdd0da6a6fe32b263
Daniel Santana de Souza <danielsantsouza94@gmail.com>
2022-04-06 17:14:10 -0300

    feat: implementa componente missionCard.jsx e renderiza a lista de missões no componente Missions.jsx

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((value) => (
+          <MissionCard
+            key={ value.year }
+            name={ value.name }
+            year={ value.year }
+            country={ value.country }
+            destination={ value.destination }
+          />
+        ))}
       </div>
     );
   }
