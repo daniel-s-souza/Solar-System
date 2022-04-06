2bd415a2012437524ba7e47b06ba8ccefbba13c3
Daniel Santana de Souza <danielsantsouza94@gmail.com>
2022-04-06 16:31:05 -0300

    feat: implementa lista de planetas no componente SolarSystem

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
+    console.log(this.props);
     return (
       <div data-testid="planet-card">
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
